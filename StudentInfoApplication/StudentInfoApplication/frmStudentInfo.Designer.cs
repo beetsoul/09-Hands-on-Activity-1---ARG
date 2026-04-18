a13c6d6a376f843b6bb142ce40a7d5b9a7b8dbe4
Jroo <159931577+beetsoul@users.noreply.github.com>
2026-04-18 09:30:09 +0800

    Add files via upload

---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace StudentInfoApplication
+{
+    partial class frmStudentInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textboxstdID = new System.Windows.Forms.TextBox();
+            this.textboxLN = new System.Windows.Forms.TextBox();
+            this.textboxFN = new System.Windows.Forms.TextBox();
+            this.submitbtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID *";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last name *";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(416, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First name *";
+            // 
+            // textboxstdID
+            // 
+            this.textboxstdID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textboxstdID.Location = new System.Drawing.Point(16, 41);
+            this.textboxstdID.Name = "textboxstdID";
+            this.textboxstdID.Size = new System.Drawing.Size(196, 20);
+            this.textboxstdID.TabIndex = 3;
+            // 
+            // textboxLN
+            // 
+            this.textboxLN.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textboxLN.Location = new System.Drawing.Point(218, 41);
+            this.textboxLN.Name = "textboxLN";
+            this.textboxLN.Size = new System.Drawing.Size(196, 20);
+            this.textboxLN.TabIndex = 4;
+            // 
+            // textboxFN
+            // 
+            this.textboxFN.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textboxFN.Location = new System.Drawing.Point(420, 41);
+            this.textboxFN.Name = "textboxFN";
+            this.textboxFN.Size = new System.Drawing.Size(196, 20);
+            this.textboxFN.TabIndex = 5;
+            // 
+            // submitbtn
+            // 
+            this.submitbtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.submitbtn.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.submitbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.submitbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.submitbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitbtn.Location = new System.Drawing.Point(528, 67);
+            this.submitbtn.Name = "submitbtn";
+            this.submitbtn.Size = new System.Drawing.Size(88, 30);
+            this.submitbtn.TabIndex = 6;
+            this.submitbtn.Text = "Submit";
+            this.submitbtn.UseVisualStyleBackColor = false;
+            this.submitbtn.Click += new System.EventHandler(this.submitbtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(35, 133);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 184);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(228, 133);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(177, 184);
+            this.listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(420, 133);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(177, 184);
+            this.listBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Student ID List";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(233, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "First Name List";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(426, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Last Name List";
+            // 
+            // frmStudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(636, 338);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.submitbtn);
+            this.Controls.Add(this.textboxFN);
+            this.Controls.Add(this.textboxLN);
+            this.Controls.Add(this.textboxstdID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmStudentInfo";
+            this.Text = "Student Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textboxstdID;
+        private System.Windows.Forms.TextBox textboxLN;
+        private System.Windows.Forms.TextBox textboxFN;
+        private System.Windows.Forms.Button submitbtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StudentInfoApplication
+{
+    public partial class frmStudentInfo : Form
+    {
+        private String studentId;
+        private String lastname;
+        private String firstname;
+
+        public frmStudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        public frmStudentInfo(string studentId, string lastname, string firstname)
+        {
+            this.studentId = studentId;
+            this.lastname = lastname;
+            this.firstname = firstname;
+        }
+
+        public String StudentID
+        {
+            get { return this.studentId; } 
+            set { this.studentId = value; }
+        }
+
+        public String LastName
+        {
+            get { return this.lastname; }
+            set { this.lastname = value; }
+        }
+
+        public String FirstName
+        {
+            get { return this.firstname; }
+            set { this.firstname = value; }
+        }
+
+        private void submitbtn_Click(object sender, EventArgs e)
+        {
+
+            frmStudentInfo std = new frmStudentInfo(
+                textboxstdID.Text, textboxLN.Text, textboxFN.Text
+                );
+
+            if (string.IsNullOrWhiteSpace(textboxstdID.Text) 
+                || string.IsNullOrWhiteSpace(textboxFN.Text) 
+                || string.IsNullOrWhiteSpace(textboxLN.Text))
+            {
+                MessageBox.Show("Please enter all student information.",
+                    "Missing Input",
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning);
+                return;
+            } else
+            {
+                listBox1.Items.Add(std.studentId);
+                listBox2.Items.Add(std.firstname);
+                listBox3.Items.Add(std.lastname);
+            }
+
+        }
+    }
+}
