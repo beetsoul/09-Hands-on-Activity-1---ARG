846da4886745258b3d09dbf29161d23e68007b67
Jroo <159931577+beetsoul@users.noreply.github.com>
2026-04-18 09:41:32 +0800

    Add files via upload

---
+++ StudentInfoApplication/StudentInfoApplication/StudentInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoApplication
+{
+    internal class StudentInfo
+    {
+
+        private String studentId;
+        private String lastname;
+        private String firstname;
+
+        public StudentInfo(string studentId, string lastname, string firstname)
+        {
+            this.studentId = studentId;
+            this.lastname = lastname;
+            this.firstname = firstname;
+        }
+
+        public String StudentID
+        {
+            get { return this.studentId; }
+            set { this.studentId = value; }
+        }
+
+        public String LastName
+        {
+            get { return this.lastname; }
+            set { this.lastname = value; }
+        }
+
+        public String FirstName
+        {
+            get { return this.firstname; }
+            set { this.firstname = value; }
+        }
+    }
+}
--- StudentInfoApplication/StudentInfoApplication/frmStudentInfo.cs
+++ StudentInfoApplication/StudentInfoApplication/frmStudentInfo.cs
@@ -23,35 +23,12 @@ namespace StudentInfoApplication
             InitializeComponent();
         }
 
-        public frmStudentInfo(string studentId, string lastname, string firstname)
-        {
-            this.studentId = studentId;
-            this.lastname = lastname;
-            this.firstname = firstname;
-        }
-
-        public String StudentID
-        {
-            get { return this.studentId; } 
-            set { this.studentId = value; }
-        }
-
-        public String LastName
-        {
-            get { return this.lastname; }
-            set { this.lastname = value; }
-        }
-
-        public String FirstName
-        {
-            get { return this.firstname; }
-            set { this.firstname = value; }
-        }
+        
 
         private void submitbtn_Click(object sender, EventArgs e)
         {
 
-            frmStudentInfo std = new frmStudentInfo(
+            StudentInfo std = new StudentInfo(
                 textboxstdID.Text, textboxLN.Text, textboxFN.Text
                 );
 
@@ -66,9 +43,9 @@ namespace StudentInfoApplication
                 return;
             } else
             {
-                listBox1.Items.Add(std.studentId);
-                listBox2.Items.Add(std.firstname);
-                listBox3.Items.Add(std.lastname);
+                listBox1.Items.Add(std.StudentID);
+                listBox2.Items.Add(std.FirstName);
+                listBox3.Items.Add(std.LastName);
             }
 
         }
